fbf51f01c38419cca25ba3079ba0df31b6a17d40
lautaro <you@example.com>
2023-09-07 20:07:20 -0300

    First commit

---
+++ src/projects.ts
@@ -0,0 +1,15 @@
+export const projects = [{
+  name: 'Save Reddit post to Markdown',
+  description: 'Este proyecto toma una URL para posterior guardar un archivo .md (Markdown)',
+  link: 'https://github.com/Lauta-dev/reddit-post-to-markdown',
+  page: 'https://lauta-dev.github.io/reddit-post-to-markdown',
+  preview: 'https://raw.githubusercontent.com/Lauta-dev/borrar/main/webPage.png',
+  api: null
+}, {
+  name: 'Anime Directory',
+  description: 'Cree una wiki de Anime y Manga usando la API de Jikan.moe. Este proyecto cuenta con autenticacion de Google proveida por Supabase',
+  link: 'https://github.com/Lauta-dev/anime-directory',
+  page: 'https://anime-directory-nu.vercel.app/',
+  preview: 'https://raw.githubusercontent.com/Lauta-dev/borrar/main/webPage.png',
+  api: 'https://jikan.moe/'
+}]
